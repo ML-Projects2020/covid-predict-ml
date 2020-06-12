eb40c014862b0ee99d7a543f3fe92161a8e2cd1e
Jasmine <titustraining4@gmail.com>
2020-06-12 15:32:12 +0530

    Files for prediction

---
+++ CovidCases.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from datetime import date
+import pickle
+import io
+import os
+cases = pd.read_csv('Ploynomial\Book.csv')
+
+cases = cases[cases['Cases'].notna()]
+
+X = cases[['Day']]
+y = cases['Cases']
+
+
+#Just to check how Linear regression works
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=100)
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+y_pred = lm.predict(X_train)
+plt.plot(X_train,y_pred,color='g',label='Linear Regression')
+
+
+polynomial_features= PolynomialFeatures(degree=3)
+x_poly = polynomial_features.fit_transform(X)
+poly = LinearRegression()
+poly.fit(x_poly, y)
+y_poly_pred = poly.predict(x_poly)
+plt.scatter(X, y, color="b",s=15)
+plt.plot(X,y_poly_pred,color='r',label='Polynomial Regression')
+
+
+r2 = metrics.r2_score(y,y_poly_pred)
+print(r2)
+#Give input of date in year, month, day
+def calculateDays():
+    f_date = date(2020, 3, 1)
+    l_date = date(2020, 6, 11) #Selected date
+    delta = l_date - f_date
+    return delta.days+1
+
+def findForNthDay(n):
+    x_poly = polynomial_features.fit_transform(n.reshape(-1,1))
+    predicts = poly.predict(x_poly)
+    return predicts
+
+
+# Creating a pickle file for the classifier
+filename = 'covid-predict-ml\polynomial_features.pkl'
+filename_model = 'covid-predict-ml\ploynomail-model.pkl'
+print(pickle)
+pickle.dump(polynomial_features, open(filename, 'wb')) 
+pickle.dump(poly, open(filename_model, 'wb')) 
+
+def linePlot():
+    casesFilter = []
+    
+    for (index, columnData) in cases.iterrows():
+        if(index % 5 == 0 ):
+            casesFilter.append(cases.iloc[index,:])
+    dataframeFilter = pd.DataFrame(np.array(casesFilter),columns = ['Date', 'Day','Cases'])
+    #convert Cases column type to float
+    dataframeFilter.Cases = dataframeFilter.Cases.astype(float)
+    #convert Date column type to datetime
+    dataframeFilter.Date = dataframeFilter.Date.astype('datetime64[ns]')
+    dataframeFilter['Date'] = dataframeFilter['Date'].map(lambda x:x.date())
+    print('linePlot', dataframeFilter)
+    plt.subplots(figsize=(11, 9))
+    sns.lineplot(x="Date", y="Cases", data=dataframeFilter)
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    my_path = "covid-predict-ml\static\images"
+    my_file = 'graph.png'
+    plt.savefig(os.path.join(my_path, my_file))
+    return "\static\images\graph.png"
---
+++ app.py
@@ -0,0 +1,44 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from CovidCases import linePlot as linePlot
+from datetime import date
+# Load the Random Forest CLassifier model
+polynomial_features = pickle.load(open("polynomial_features.pkl", 'rb'))
+ploynomail_model = pickle.load(open("ploynomail-model.pkl", 'rb'))
+print("ploy_features", polynomial_features)
+print("ploynomail-model", ploynomail_model)
+
+app = Flask(__name__)
+
+def calculateDays(year, month, day):
+    f_date = date(2020, 3, 1)
+    l_date = date(year, month, day) #Selected date
+    delta = l_date - f_date
+    return np.array(delta.days+1)
+
+@app.route('/')
+def home():
+    linePlotPath = linePlot()
+    print(linePlotPath)
+    return render_template('index.html', linePlotPath=linePlotPath)
+    
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        date = str(request.form['date'])
+        dates = date.split('-')
+        print("Dates", int(dates[0]))
+        noOfdays = calculateDays(int(dates[0]),int(dates[1]), int(dates[2]))
+        x_poly = polynomial_features.fit_transform(noOfdays.reshape(-1,1))
+        my_prediction = ploynomail_model.predict(x_poly)
+        predictNumber = int(my_prediction)
+        print('myprediction', my_prediction)
+        fromNumber = predictNumber - 7500
+        toNumber = predictNumber + 7500
+        
+        return render_template('result.html', fromNumber=fromNumber, toNumber=toNumber)
+
+if __name__ == '__main__':
+	app.run(debug=True)
